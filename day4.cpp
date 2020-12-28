0ff4f7cca80ded111cd3d3c3835e2e3dd753cda5
Jacob Toomey <jacobtoomey@pop-os.localdomain>
2020-12-28 11:02:11 -0800

    day4 finished

--- day4.cpp
+++ day4.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <array>
 #include <sstream>
+#include <regex>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,7 +23,54 @@ enum keys{
 	KEYS_LENGTH,
 };
 
+bool valid_eyecolor(string color)
+{
+		string colors[7] = {"amb", "blu",
+							"brn", "gry",
+							"grn", "hzl",
+							"oth"};
 
+		
+		if(color.length() != 3)
+			return false;
+
+		for(int i = 0; i < 8; i++)
+		{
+			if(color.compare(colors[i]) == 0)
+				return true;
+		}
+
+		return false;
+
+}
+
+bool valid_pid(string id)
+{
+	if(id.length() != 9)
+		return false;
+	
+	for(auto iter = id.begin(); iter < id.end(); iter++)
+	{
+		if(!isdigit(*iter))
+			return false;
+	}
+
+	return true;
+}
+
+bool valid_four_digit(string num, int max, int min)
+{
+	if(num.length() != 4)
+		return false;
+	
+	for(int i = 0; i < num.length(); i++)
+	{
+		if(!isdigit(num[i]))
+			return false;
+	}
+
+	return (atoi(num.c_str()) > max || atoi(num.c_str()) < min) ? false : true;
+}
 int key_to_int(string str)
 {
 	if(str.compare("byr") == 0)
@@ -55,25 +103,63 @@ int key_to_int(string str)
 bool valid_passport(vector<string> &passport)
 {
 	cout << "Passport\n";
-	bool valid = true;
 	for(int i = 0; i < 8; i++)
 	{
 			cout << "| " << i << "->" << passport[i] << "|\n";
-			if(i != CID && passport[i].empty()){
-				valid = false;
-			}
-			if( i == EYR && (stoi(passport[i]) <= 2020) || (stoi(passport[i]) >= 2030))
-				valid = false;
-
-			if( i == BYR && (stoi(passport[i]) <= 1920 ) || (stoi(passport[i]) >= 2002))			
-				valid = false;
+			if(i != CID && passport[i].empty())
+				return false;
 			
-			if( i == IYR && (stoi(passport[i]) <= 2010) || (stoi(passport[i]) >= 2020 ))
-				valid = false;
+			if( i == EYR && !valid_four_digit(passport[i], 2030, 2020))
+				return false;
 
+			if( i == BYR &&	!valid_four_digit(passport[i] , 2002, 1920 ))
+				return false;
+
+			if( i == IYR && !valid_four_digit(passport[i], 2020, 2010))
+				return false;
+
+			if( i == HGT)
+			{
+				string units = passport[i].substr(passport[i].length() - 2, passport[i].length());
+				string snum = passport[i].substr(0, passport[i].length() - 2);
+				int num;
+
+				if(units.compare("cm") == 0){
+					num = atoi(snum.c_str());
+					if(num < 150 || num > 193)
+						return false;
+				}
+				else if(units.compare("in") == 0){
+					num = atoi(snum.c_str());
+					if(num < 59 || num > 76)
+						return false;
+				}
+				else{
+					return false;
+				}
+
+			}
+
+			if( i == HCL )
+			{
+				regex e("(^#)([0-9a-f]{6})");
+				smatch match;
+
+				if(!regex_search(passport[i], match, e))
+				{
+					return false;
+				}
+				
+			}
+			if(i == ECL && (!valid_eyecolor(passport[i])))
+				return false;
+			
+			if(i == PID && (!valid_pid(passport[i])))
+				return false;
 			
 	}
-	return valid;
+	cout << "Valid\n";
+	return true;
 }
 
 void clear_passport(vector<string> &passport)
